0e114c66c29af234a4c81fbc36771bfc28cd9b52
aswin-kevin <aswinvenkat60@gmail.com>
2022-11-09 13:39:59 +0530

    created instagram user finder

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import NextCors from "nextjs-cors";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  // console.log("Somebody requested for a curl conversion", req);
+  const { ipAddress, time } = req.body;
+  await NextCors(req, res, {
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+  console.log("Someone sent hello from :", ipAddress, "at", time);
+  res.status(200).json({ resp: 200 });
 }
